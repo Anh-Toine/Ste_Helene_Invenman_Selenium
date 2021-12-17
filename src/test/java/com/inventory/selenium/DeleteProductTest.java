5c3fbfc061bc68741edde8b6dd0e12d4ce7d32ff
Anh-Toine <Nick1234567890a@gmail.com>
2021-12-16 19:14:39 -0500

    - Branched out the delete_product_test test case such that it covers the entire scope of the system(add,update, and delete).

--- src/test/java/com/inventory/selenium/DeleteProductTest.java
+++ src/test/java/com/inventory/selenium/DeleteProductTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
